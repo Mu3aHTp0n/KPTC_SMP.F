8cc495733ccb236d882af83aa29eff5657d08811
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-03-05 16:08:13 +0300

    refactor: home page

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 
-import MainHeader from './MainHeader'
-import HomePage from './home/HomePage'
-import NewsPage from './news/NewsPage'
+import MainHeader from '@widgets/MainHeader';
+import HomePage from './home/HomePage';
+import NewsPage from '@pages/NewsPage/NewsPage';
+import GuildPage from '@pages/GuildPage/GuildPage';
+import HowToPlayPage from '@pages/HowToPlayPage/HowToPlayPage';
 
 export default function MainPage() {
-  return (
-    <>
-        <MainHeader />
-        <Routes>
-            <Route path='/'     element={<HomePage />} />
-            <Route path='/news' element={<NewsPage />} />
-        </Routes>
-    </>
-  )
+	return (
+		<>
+			<MainHeader />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/news" element={<NewsPage />} />
+				<Route path="/guild" element={<GuildPage />} />
+				<Route path="/how-to-play" element={<HowToPlayPage />} />
+			</Routes>
+		</>
+	);
 }
