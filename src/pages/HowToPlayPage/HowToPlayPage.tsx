ad5bab3b4cd877a441ef8c54424f3f23fcaaf110
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-03-05 15:41:04 +0300

    refactor: how to play page (in progress)

---
+++ src/pages/HowToPlayPage/HowToPlayPage.tsx
@@ -0,0 +1,7 @@
+export default function HowToPlayPage() {
+	return (
+		<div className="bg-[#191919] min-h-[100vh] pt-8 text-white flex justify-center items-center">
+			<h1 className="text-blue-700 text-[300px] font-bold">Cumинг сун</h1>
+		</div>
+	);
+}
