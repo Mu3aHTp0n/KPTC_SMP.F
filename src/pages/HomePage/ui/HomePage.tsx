b0465c80a62108326ed2a4d6fa0d83c9f1296096
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-05-28 18:55:18 +0300

    feature: new content to home page

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,17 +1,32 @@
 import { Skins } from '@shared/ui/Skins';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons/faCopy';
+import { Outlet } from 'react-router-dom';
 
 import dockychan from '../assets/Докчанский.png';
 import zigMan from '../assets/ЗМ.png';
 import diamond from '../assets/diamond.png';
 import leftFire from '../assets/Жёлтое_пламя.png';
 import rightFire from '../assets/Красное_пламя.png';
+import MatrixEdik from '../assets/MatrixEdik.png'
+import voiceChat from '../assets/Войс и эмоции.png'
+
+import styles from './HomePage.module.scss'
+import { minecraftServerApi } from '@shared/constants/minecraftServerApi';
 
 export default function HomePage() {
+
+	const copyServerIp = async () => {
+		await navigator.clipboard.writeText(minecraftServerApi);
+		alert('Успешно скопировано')
+	}
+
 	return (
-		<div className='bg-[#191919] pt-8'>
+		<main className={styles.content}>
 			<div className='container max-w-[1200px] mx-auto'>
-				<Skins/>
-				<section className='relative flex justify-between items-center mt-8 text-white bg-[#111111] rounded-2xl overflow-hidden'>
+				<Outlet/>
+				<Skins />
+				<section className={styles.about}>
 					<div className='bg-[url(../shared/Жёлтое_пламя.png)]'>
 						<img
 							className='absolute select-none left-0 h-full'
@@ -21,7 +36,7 @@ export default function HomePage() {
 							alt=''
 						/>
 						<img
-							className='relative select-none z-10 pt-3'
+							className={styles.playerImage}
 							draggable={false}
 							src={dockychan}
 							alt='Dockychan'
@@ -33,10 +48,14 @@ export default function HomePage() {
 							<h3>About</h3>
 							<img className='w-6 h-6' src={diamond} alt='*' />
 						</div>
-						<div className='relative z-20 text-nowrap'>
+						<div className='relative z-20 text-base text-nowrap'>
 							<p>KPTC SMP - проект, где игрок сам выбирает свою судьбу.</p>
 							<p>Объединяйся с другими игроками, или развивайся в одиночку.</p>
 							<p>Присоединяйся к серверу уже сейчас. </p>
+							<code>
+								{minecraftServerApi}
+								<FontAwesomeIcon onClick={copyServerIp} className={'ml-2 cursor-pointer'} icon={faCopy} />
+							</code>
 						</div>
 					</div>
 					<div>
@@ -48,14 +67,30 @@ export default function HomePage() {
 							alt=''
 						/>
 						<img
-							className='relative select-none z-10 pt-3 h-full'
+							className={styles.playerImage}
 							draggable={false}
 							src={zigMan}
 							alt='ZigMan'
 						/>
 					</div>
 				</section>
+				<section className={'flex justify-around'}>
+					<div className={'flex-2 pt-32'}>
+						<h3 className={'text-2xl border-b-2 border-solid'}>Ролевые игры</h3>
+						<p>Создавайте персонажей и истории</p>
+					</div>
+					<img className={styles.matrixImage} src={MatrixEdik} alt='' />
+				</section>
+				<section className={styles.voiceSection}>
+					<img className={'flex-3 max-w-[600px]'} src={voiceChat} alt='' />
+					<div className={'pt-32'}>
+						<h3 className={'text-2xl border-b-2 border-solid'}>
+							Голосовой чат и эмоции
+						</h3>
+						<p>Общайся и выражай эмоции</p>
+					</div>
+				</section>
 			</div>
-		</div>
+		</main>
 	);
 }
